bd3366d61b7c6056997702056437c4aaa220e679
Kamei Shogo <km45@users.noreply.github.com>
2019-07-07 02:22:28 +0900

    Add pod cases

--- pod/pod.cc
+++ pod/pod.cc
@@ -5,11 +5,11 @@
 //-----------------------------------------------------------------------------
 // Utility
 //-----------------------------------------------------------------------------
-#define EXPECT_POD(TYPE) \
-  static_assert(std::is_pod<TYPE>::value, #TYPE " should be POD.")
+#define EXPECT_POD(...) \
+  static_assert(std::is_pod<__VA_ARGS__>::value, #__VA_ARGS__ " should be POD.")
 
-#define EXPECT_NON_POD(TYPE) \
-  static_assert(!std::is_pod<TYPE>::value, #TYPE " should be non-POD.")
+#define EXPECT_NON_POD(...) \
+  static_assert(!std::is_pod<__VA_ARGS__>::value, #__VA_ARGS__ " should be non-POD.")
 
 #define EXPECT_TRIVIAL(TYPE) \
   static_assert(std::is_trivial<TYPE>(), #TYPE " should be trivial.")
@@ -40,6 +40,9 @@ struct NonTrivial {
   NonTrivial(const NonTrivial&) = default;
 };
 EXPECT_NON_POD(NonTrivial);
+
+EXPECT_POD(int[]);
+EXPECT_POD(std::array<int, 2>);
 
 struct RawArray {
   int x[2];
